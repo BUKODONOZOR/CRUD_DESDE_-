25e7ad98136a89097878f170ebf81b60f556eb85
BUKODONOZOR <90481288+BUKODONOZOR@users.noreply.github.com>
2022-02-23 21:31:13 -0500

    Punto inicial del proyecto

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>PETICION BASICA</h1>
+      <div classHooksName="flex-row">
+        <div className="flex-large">
+          <h2>Agregar usuario</h2>
+        </div>
+        <div className="flex-large">
+          <h2>Ver usuarios</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
